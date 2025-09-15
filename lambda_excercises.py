42c7dc2b33e94553b517573a279ec65aad5ed550
KasraHastam <kasra.danesh2021@gmail.com>
2025-09-15 22:59:16 +0330

    Add files via upload

---
+++ lambda_excercises.py
@@ -0,0 +1,13 @@
+#Lambda,Map,Filter,Zip 
+
+# Tavan3 = lambda x: x*x*x
+# print(Tavan3(4))
+
+# Numbers = [122,24,232,45,1,3,8,5,32,53,9,2,4]
+# Certain_Numbers = list(filter(lambda x: x>10 , Numbers))
+# print(Certain_Numbers)
+
+# Names = ["Ali","Kasra","Mehdi"]
+# Grades = [20,12,18]
+# Zipping = dict(zip(Names , Grades))
+# print(Zipping)
